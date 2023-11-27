8f8d2b2968695c7f7f54bc2196275d1ac631c07a
enricomazzotti <77339732+enricomazzotti@users.noreply.github.com>
2023-11-27 12:09:44 +0100

    phase 1 pcb

--- phase1/pcb.c
+++ phase1/pcb.c
@@ -5,30 +5,72 @@ LIST_HEAD(pcbFree_h);            /* List of free PCBs                     */
 static int next_pid = 1;
 
 void initPcbs() {
+    for (int i=0;i<MAXPROC;i++){
+        pcb_t *p = &pcbTable[i];
+        list_add(&p->p_list,&pcbFree_h);
+    }
 }
 
 void freePcb(pcb_t *p) {
+    list_add(&p->p_list,&pcbFree_h);
 }
 
 pcb_t *allocPcb() {
+    if (list_empty(&pcbFree_h))
+        return NULL;
+    pcb_t *p = container_of(pcbFree_h.next,pcb_t,p_list);
+    list_del(&p->p_list);
+    p->p_parent=NULL;
+    p->p_child.next=NULL;
+    p->p_child.prev=NULL;
+    p->msg_inbox.next=NULL;
+    p->msg_inbox.prev=NULL;
+    p->p_pid=0;
+    p->p_s.cause=0;
+    p->p_s.entry_hi=0;
+    for (int i = 0; i < 32; i++)
+        p->p_s.gpr[i]=0;
+    p->p_s.mie=0;
+    p->p_s.pc_epc=0;
+    p->p_s.status=0;
+    p->p_sib.next=NULL;
+    p->p_sib.next=NULL;
+    p->p_supportStruct=NULL;
+    p->p_time=0;
+    return p;
 }
 
 void mkEmptyProcQ(struct list_head *head) {
+    INIT_LIST_HEAD(head);
 }
 
 int emptyProcQ(struct list_head *head) {
+    return list_empty(head);
 }
 
 void insertProcQ(struct list_head* head, pcb_t* p) {
+    list_add_tail(&(p->p_list), head);
 }
 
 pcb_t* headProcQ(struct list_head* head) {
+    if (emptyProcQ(head))
+        return NULL;
+    return container_of(head->next,pcb_t,p_list);
 }
 
 pcb_t* removeProcQ(struct list_head* head) {
+    if (emptyProcQ(head))
+        return NULL;
+    pcb_t *p = container_of(head->next,pcb_t,p_list);
+    list_del(head->next);
+    return p;
 }
 
 pcb_t* outProcQ(struct list_head* head, pcb_t* p) {
+    if (emptyProcQ(head))
+        return NULL;
+    list_del(&(p->p_list));
+    return p;
 }
 
 int emptyChild(pcb_t *p) {
