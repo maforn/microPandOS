314f8ed2b7bbd8396683812efabbfc7493963720
Mattia Ferrarini <mattia.ferrarini3@studio.unibo.it>
2024-02-22 11:32:08 +0100

    Added utils and part of syscalls

--- phase2/exceptions.c
+++ phase2/exceptions.c
@@ -46,13 +46,17 @@ requested in user-mode. This is done by setting Cause.ExcCode in the stored exce
 (Reserved Instruction), and calling one’s Program Trap exception handler.
 Technical Point: As described above [Section 4], the saved exception state (for Processor 0) is
 stored at the start of the BIOS Data Page (0x0FFF.F000) [Section 3.2.2-pops].*/
+			// TODO: find RI
+			setCAUSE(2);
+			exceptionHandler();
 			}
 			else {
-				if (proc_state->reg_a0 == SENDMESSAGE) {
-					
-				}
-				else if (proc_state->reg_a0 == RECEIVEMESSAGE) {
-
+				if (proc_state->reg_a0 == SENDMESSAGE)
+					SendMessage(proc_state);
+				else if (proc_state->reg_a0 == RECEIVEMESSAGE)
+					ReceiveMessage(proc_state);
+				else{
+					// Pass up
 				}
 			}
 		}
@@ -69,4 +73,50 @@ stored at the start of the BIOS Data Page (0x0FFF.F000) [Section 3.2.2-pops].*/
 	0 8-11 Syscall
 	0 24-28 TLB exceptions */
 
+}
+
+void SendMessage(state_t *proc_state){
+	pcb_t *dst = (pcb_t *)proc_state->reg_a1;
+
+	// dst doesn't exist
+	if(contains(pcbFree_h, dst->p_list)){
+		proc_state->reg_a0 = DEST_NOT_EXIST; 
+	}
+	// dst is waiting for a message from current process
+	else if(contains(blocked_pcbs[SEMDEVLEN][1], dst->p_list)
+			&& ((dst->p_s).reg_a1 == ANYMESSAGE || (dst->p_s).reg_a1 == current_process)){
+
+		// copy message and sender in designated memory areas
+		memcpy((dst->p_s).reg_a2, &proc_state->reg_a2, sizeof(unsigned int));
+		memcpy((dst->p_s).reg_a0, &current_process->p_pid, sizeof(int));
+			
+		// awake receveing process and update count
+		insertProcQ(&ready_queue, dst); 
+		soft_block_count--;
+	}
+	else{ // dst is in ready queue or (waiting for I/O or timer) or waiting for message from different sender
+		msg_t *msg = allocMsg();
+
+		if(msg == NULL){ // no available messages
+			proc_state->reg_a0 = MSGNOGOOD;
+		}
+		else{
+			msg->m_payload = proc_state->reg_a2; 
+			msg->m_sender = current_process;
+
+			pushMessage(&dst->msg_inbox, msg);
+			proc_state->reg_a0 = 0;
+		}
+	}
+}
+void ReceiveMessage(state_t *proc_state){
+	pcb_t *sender = proc_state->reg_a1;
+	msg_t *msg = popMessage(&current_process->msg_inbox, sender);
+	if (msg == NULL){
+
+	}
+	else{
+		proc_state->reg_a0 = msg->m_sender->p_pid;
+		memcpy(proc_state->reg_a2, &msg->m_payload, sizeof(unsigned int));
+	}
 }
--- phase2/headers/utils.h
+++ phase2/headers/utils.h
@@ -1,7 +1,13 @@
-#ifndef UTILS_H_INCLUDED
-#define UTILS_H_INCLUDED
+#ifndef SSI_H_INCLUDED
+#define SSI_H_INCLUDED
 
-void *memcpy();
+#include "../../headers/listx.h"
+
+// returns 1 if elem is in the list whose sentinel is head. Returns 0 otherwise
+int contains(struct list_head head, struct list_head elem);
+
+// implements C memcpy
+void *memcpy(void *dst, const void *src, unsigned int len);
 void debug(void *arg);
 
 #endif
