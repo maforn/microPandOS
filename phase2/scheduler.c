78561c90d6946408a4abab0b46e06ef4743c2c78
Mattia Ferrarini <mattia.ferrarini3@studio.unibo.it>
2024-03-04 09:51:33 +0100

    Guess at condition for test success

--- phase2/scheduler.c
+++ phase2/scheduler.c
@@ -3,10 +3,17 @@
 #include "./headers/initial.h"
 #include <uriscv/liburiscv.h>
 
+
+// TODO: check condition
+int ssiOnlyProcess(){
+	return process_count == 1 && (current_process == ssi_pcb || headProcQ(&ready_queue) == ssi_pcb
+		|| (soft_block_count == 1 && ssi_pcb->blocked==1));
+}
+
 void schedule() {
 	if (emptyProcQ(&ready_queue)) {
 		// TODO: Controllare se è così che si controlla che SSI è l'unico vivo
-		if (process_count == 1 && current_process == ssi_pcb)
+		if (ssiOnlyProcess())
 			HALT();
 		else if (process_count > 0 && soft_block_count > 0) {
 			setTIMER(PSECOND);
