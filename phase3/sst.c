9793274b2c52a5bc33b4a4fde6184ebd3735d30f
enisb1 <b.encio.003@gmail.com>
2024-04-29 14:18:09 +0200

    set up uproc's page table

--- phase3/sst.c
+++ phase3/sst.c
@@ -7,7 +7,7 @@
 //entrambi mezza pagesize, quindi insieme avranno la size di una pagesize
 //-> avremo la prima page per gli stack del primo u-proc, la seconda per gli stack del secondo e così via...
 
-state_t* get_initial_proc_state(unsigned short index) {
+state_t* get_initial_proc_state(unsigned short procNumber) {
 	// TODO: check if values are correct
 	state_t* uproc_state = malloc(sizeof(state_t));
 	uproc_state->reg_sp = USERSTACKTOP;
@@ -16,23 +16,37 @@ state_t* get_initial_proc_state(unsigned short index) {
     	uproc_state->status = MSTATUS_MIE_MASK;
     	uproc_state->mie = MIE_ALL;
     	// set entry hi asid to i
-    	uproc_state->entry_hi = index << ASIDSHIFT;
+    	uproc_state->entry_hi = procNumber << ASIDSHIFT;
 	return uproc_state;
 }
 
-support_t* get_proc_support_struct(unsigned short index) {
+void setUpPageTable(support_t *uproc) {
+  for (int i = 0; i < USERPGTBLSIZE; i++) {
+    // TODO: check shift
+    uproc->sup_privatePgTbl[i].pte_entryHI =
+        ((UPROCSTARTADDR + i * PAGESIZE) << VPNSHIFT) +
+        (uproc->sup_asid << ASIDSHIFT);
+    uproc->sup_privatePgTbl[i].pte_entryLO = DIRTYON;
+  }
+  // set the last VPN to 0xBFFFF000
+  uproc->sup_privatePgTbl[USERPGTBLSIZE - 1].pte_entryHI =
+      ((USERSTACKTOP - PAGESIZE) << VPNSHIFT) + (uproc->sup_asid << ASIDSHIFT);
+}
+
+support_t* get_proc_support_struct(unsigned short procNumber) {
 	support_t* uproc_sup = malloc(sizeof(support_t));
 	//initialize asid
-	uproc_sup->sup_asid = index;
+	uproc_sup->sup_asid = procNumber;
 	//initialize sup_exceptContext 
 	memaddr ramtop;
 	RAMTOP(ramtop);
 	memaddr PENULTIMATE_RAM_FRAME = ramtop - PAGESIZE;
 	uproc_sup->sup_exceptContext[0] = {.pc = &support_level_TLB_handler,.status = MSTATUS_MIE_MASK + MSTATUS_MPP_M,
-	.stackPtr = PENULTIMATE_RAM_FRAME - (index-1)*PAGESIZE};
+	.stackPtr = PENULTIMATE_RAM_FRAME - (procNumber-1)*PAGESIZE};
 	uproc_sup->sup_exceptContext[1] = {.pc = &support_level_general_handler, .status = MSTATUS_MPP_M + MSTATUS_MIE_MASK,
-	.stackPtr = PENULTIMATE_RAM_FRAME - (index-1)*PAGESIZE + 0.5*PAGESIZE};
-	//TODO: initialize pgTbl
+	.stackPtr = PENULTIMATE_RAM_FRAME - (procNumber-1)*PAGESIZE + 0.5*PAGESIZE};
+	//initialize pgTbl
+	setUpPageTable(uproc_sup);
 	return uproc_sup;
 }
 
@@ -42,12 +56,12 @@ support_t* get_proc_support_struct(unsigned short index) {
  * first the corresponding child u-proc is initialized and created
  * then the SST waits for service requests from its child process
 */
-void create_SST(unsigned short index) {
+void create_SST(unsigned short procNumber) {
 	//pointer to initial processor state for the u-proc	
-	state_t* uproc_proc_state = get_initial_proc_state(index); 
+	state_t* uproc_proc_state = get_initial_proc_state(procNumber); 
 	
 	//pointer to an initialized support structure for the u-proc
-	support_t* uproc_sup = get_proc_support_struct(index);
+	support_t* uproc_sup = get_proc_support_struct(procNumber);
 
 	//launch u-proc (create_process request to SSI)
 }
