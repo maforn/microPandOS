6e08efb99c63cb1278d959f110f840b4f66238d1
Mattia Ferrarini <119322415+MattiaFerrarini@users.noreply.github.com>
2024-04-30 10:10:54 +0200

    Pager Implementation (#44)
    * TLB Refill event implemented
    
    * Implemented pager (*maybe*)
    
    * Removed bug, improved performance, added swap table initialization
    
    ---------
    
    Co-authored-by: Matteo <matteo.fornaini@studio.unibo.it>
    Co-authored-by: maforn <44122142+maforn@users.noreply.github.com>

---
+++ phase3/headers/initProc.h
@@ -0,0 +1,8 @@
+#ifndef INIT_PROC_H_INCLUDED
+#define INIT_PRCO_H_INCLUDED
+
+#include "../../phase1/headers/pcb.h"
+
+extern pcb_t *swap_mutex_pcb;           // pcb of mutex process
+
+#endif
--- phase3/initProc.c
+++ phase3/initProc.c
@@ -1,47 +1,11 @@
 #include "../headers/types.h"
 #include "../phase2/headers/initial.h"
+#include <uriscv/liburiscv.h>
+#include "./headers/initProc.h"
+
+pcb_t * swap_mutex_pcb;
+swap_t swap_table[POOLSIZE];
 
 pcb_PTR test_pcb, swap_mutex, print0_pcb, print1_pcb;
-
-/* a procedure to print on terminal 0
-void print()
-{
-    while (1)
-    {
-        char *msg;
-        unsigned int sender = SYSCALL(RECEIVEMESSAGE, ANYMESSAGE, (unsigned
-int)(&msg), 0); char *s = msg; devregtr *base = (devregtr *)(TERM0ADDR);
-        devregtr *command = base + 3;
-        devregtr status;
-
-        while (*s != EOS)
-        {
-            devregtr value = PRINTCHR | (((devregtr)*s) << 8);
-            ssi_do_io_t do_io = {
-                .commandAddr = command,
-                .commandValue = value,
-            };
-            ssi_payload_t payload = {
-                .service_code = DOIO,
-                .arg = &do_io,
-            };
-            SYSCALL(SENDMESSAGE, (unsigned int)ssi_pcb, (unsigned
-int)(&payload), 0); SYSCALL(RECEIVEMESSAGE, (unsigned int)ssi_pcb, (unsigned
-int)(&status), 0);
-
-            if ((status & TERMSTATMASK) != RECVD)
-                PANIC();
-
-            s++;
-        }
-        SYSCALL(SENDMESSAGE, (unsigned int)sender, 0, 0);
-    }
-}
-
-void print_term0(char *s)
-{
-    SYSCALL(SENDMESSAGE, (unsigned int)print_pcb, (unsigned int)s, 0);
-    SYSCALL(RECEIVEMESSAGE, (unsigned int)print_pcb, 0, 0);
-} */
 
 void test() { test_pcb = current_process; }
